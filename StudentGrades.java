409084990629321bb6fd8de87723350fc7903707
Tim Moriarty <timmoriarty@gmail.com>
2022-02-16 15:32:44 -0600

    adding initial file

---
+++ StudentGrades.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class StudentGrades {
+       
+   public static void main (String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      String studentName;
+      double studentGrade;
+      
+      //TODO: Create Hashmap of Strings to Doubles pointed to by a
+      //variable called studentGrades
+      
+      // Students's grades (pre-entered)
+      studentGrades.put("Harry Rawlins", 84.3);
+      studentGrades.put("Stephanie Kong", 91.0);
+      studentGrades.put("Shailen Tennyson", 78.6);
+      studentGrades.put("Quincy Wraight", 65.4);
+      studentGrades.put("Janine Antinori", 98.2);
+      //TODO: Please an entry for yourself along with a grade
+      
+      
+      // TODO: Read in new grade for a student, output initial 
+      //       grade, replace with new grade in HashMap,
+      //       output new grade
+      
+   }
+}
